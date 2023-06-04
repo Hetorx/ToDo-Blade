12e8b656e2c11cf5cbc293d74fff7206e8d6d811
arkadiusz.koziol <arkadiuszkoziol2@gmail.com>
2023-06-04 21:33:02 +0200

    initial laravel commit

---
+++ shop_app/app/Http/Controllers/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+    public function index() {
+        $pages = [
+            'about' => 'A little bit more information about us!',
+            'contact' => 'Contact us by facebook or instagram!',
+            'way' => "<Img which shows the best way to our office>"
+        ];
+        dd($pages);
+    }
+
+    public function show($slug) {
+        $pages = [
+            'about' => 'A little bit more information about us!',
+            'contact' => 'Contact us by facebook or instagram!',
+            'way' => "<Img which shows the best way to our office>"
+        ];
+        return $pages[$slug];
+    }
+
+}
