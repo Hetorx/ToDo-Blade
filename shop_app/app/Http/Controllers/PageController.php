a0ebef3b85f2326c6789fe8c4782091a318195e8
arkadiusz.koziol <arkadiuszkoziol2@gmail.com>
2023-06-05 22:21:53 +0200

    model

--- shop_app/app/Http/Controllers/PageController.php
+++ shop_app/app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Page;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -17,13 +18,10 @@ class PageController extends Controller
         dd($pages);
     }
     
-    public function show($slug) {
-        $pages = [
-            'about' => 'A little bit more information about us!',
-            'contact' => 'Contact us by facebook or instagram!',
-            'way' => "<Img which shows the best way to our office>"
-        ];
-        return $pages[$slug];
+    public function show($id) {
+        $page = Page::find ($id);
+
+        return $page;
     }
 
 }
